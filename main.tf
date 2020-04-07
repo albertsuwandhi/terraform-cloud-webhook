334e0a1f2cb93b7db35891b249bc021775aabf49
Albert Suwandhi <albert.suwandhi@gmail.com>
2020-04-07 15:08:21 +0700

    add new s3 bucket --> trigger webhook

--- main.tf
+++ main.tf
@@ -12,3 +12,13 @@ resource "null_resource" "local_run" {
         command = "ls -l; whoami; uname -a; sleep 10"
     }
 }
+
+resource "aws_s3_bucket" "covid19webhook" {
+  bucket = "covid19webhook"
+  acl    = "private"
+
+  tags = {
+    Name        = "covid19"
+    Environment = "Dev"
+  }
+}
